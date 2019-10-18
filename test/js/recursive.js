e1143dd76c52521a93ffca9a23ca560848debdfc
YuelongLi <cloudnest.liyuelong@gmail.com>
2019-10-18 01:31:33 -0400

    Cleanup unrelated files

--- test/js/recursive.js
+++
@@ -1,3 +0,0 @@
-search = (n, array=[], index = Math.floor(array.length/2))=>(n<array[index])?search(n, array, Math.floor(index / 2)): (n > array[index]) ?search(n, array, Math.floor(array.length * 3 / 2)): index;
-
-console.log(search(3, [2,1,3,5]));
